1e80559ee6021971dfdcdf0b23ebf26d9b03a6dc
LuckyUs <briandreleung@gmail.com>
2018-02-15 00:22:00 +0800

    1st attempt

---
+++ routes/sensors.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var resources = require('./../resources/model');
+
+router.route('/').get((req, res, next) => {
+	res.send(resources.pi.sensors);
+	// req.result(resources.pi.sensors);
+	// next();
+});
+
+router.route('/temperature').get((req, res, next) => {
+	res.send(resources.pi.sensors.temperature);
+	// req.result(resources.pi.sensors.temperature);
+	// next();
+});
+
+router.route('/humidity').get((req, res, next) => {
+	res.send(resources.pi.sensors.humidity);
+	// req.result(resources.pi.sensors.humidity);
+	// next();
+});
+
+module.exports = router;
