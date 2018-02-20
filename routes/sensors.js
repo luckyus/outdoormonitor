b96ed273fc0175d90dead018a524fdcbbc0cbe68
LuckyUs <briandreleung@gmail.com>
2018-02-20 13:00:07 +0800

    init

--- routes/sensors.js
+++ routes/sensors.js
@@ -3,21 +3,21 @@ var router = express.Router();
 var resources = require('./../resources/model');
 
 router.route('/').get((req, res, next) => {
-	res.send(resources.pi.sensors);
-	// req.result(resources.pi.sensors);
-	// next();
+	// res.send(resources.pi.sensors);
+	req.result(resources.pi.sensors);
+	next();
 });
 
 router.route('/temperature').get((req, res, next) => {
-	res.send(resources.pi.sensors.temperature);
-	// req.result(resources.pi.sensors.temperature);
-	// next();
+	// res.send(resources.pi.sensors.temperature);
+	req.result(resources.pi.sensors.temperature);
+	next();
 });
 
 router.route('/humidity').get((req, res, next) => {
-	res.send(resources.pi.sensors.humidity);
-	// req.result(resources.pi.sensors.humidity);
-	// next();
+	// res.send(resources.pi.sensors.humidity);
+	req.result(resources.pi.sensors.humidity);
+	next();
 });
 
 module.exports = router;
