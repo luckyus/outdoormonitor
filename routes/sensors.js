ff98fc5d09662132d9053db365d8329677f6e9e0
LuckyUs <briandreleung@gmail.com>
2018-02-23 14:01:41 +0800

    req.result = blah blah blah

--- routes/sensors.js
+++ routes/sensors.js
@@ -4,19 +4,19 @@ var resources = require('./../resources/model');
 
 router.route('/').get((req, res, next) => {
 	// res.send(resources.pi.sensors);
-	req.result(resources.pi.sensors);
+	req.result = resources.pi.sensors;
 	next();
 });
 
 router.route('/temperature').get((req, res, next) => {
 	// res.send(resources.pi.sensors.temperature);
-	req.result(resources.pi.sensors.temperature);
+	req.result = resources.pi.sensors.temperature;
 	next();
 });
 
 router.route('/humidity').get((req, res, next) => {
 	// res.send(resources.pi.sensors.humidity);
-	req.result(resources.pi.sensors.humidity);
+	req.result = resources.pi.sensors.humidity;
 	next();
 });
 
