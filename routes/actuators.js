bd5e939bf30c1eb1c199aa2dd49da2879954be91
LuckyUs <briandreleung@gmail.com>
2018-02-28 15:53:55 +0800

    ljljl

--- routes/actuators.js
+++ routes/actuators.js
@@ -3,28 +3,28 @@ var router = express.Router();
 var resources = require('./../resources/model');
 
 
-router.route('/').get(function (req, res, next) {
-	req.result = resources.pi.actuators;
-	next();
+router.route('/').get(function(req, res, next) {
+    req.result = resources.pi.actuators;
+    next();
 });
 
-router.route('/leds').get(function (req, res, next) {
-	req.result = resources.pi.actuators.leds;
-	next();
+router.route('/leds').get(function(req, res, next) {
+    req.result = resources.pi.actuators.leds;
+    next();
 });
 
-router.route('/leds/:id').get(function (req, res, next) {
-	req.result = resources.pi.actuators.leds[req.params.id];
-	console.log('get: ' + JSON.stringify(req.result));
-	next();
-}).put(function (req, res, next) {
-	console.log(resources.pi.actuators.leds[req.params.id]);
-	var selectedLed = resources.pi.actuators.leds[req.params.id];
-	selectedLed.value = req.body.value;
-	req.result = selectedLed;
-	console.log('put: ' + JSON.stringify(req.result));
-	next();
+router.route('/leds/:id').get(function(req, res, next) {
+    req.result = resources.pi.actuators.leds[req.params.id];
+    console.log('get: ' + JSON.stringify(req.result));
+    next();
+}).put(function(req, res, next) {
+	console.log('existing: ' + resources.pi.actuators.leds[req.params.id]);
+	console.log('req.body.value: ' + req.body.value);
+    var selectedLed = resources.pi.actuators.leds[req.params.id];
+    selectedLed.value = req.body.value;
+    req.result = selectedLed;
+    console.log('put: ' + JSON.stringify(req.result));
+    next();
 });
 
 module.exports = router;
-
