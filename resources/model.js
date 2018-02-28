e9960f3659ff4cc16c66a7f1ff6fa5630a52a47b
LuckyUs <briandreleung@gmail.com>
2018-02-28 18:29:33 +0800

    testing proxy in model.js

--- resources/model.js
+++ resources/model.js
@@ -1,2 +1,13 @@
 var resources = require('./resources.json');
-module.exports = resources;
+
+var handler = {
+    get(target, key) {
+		console.log(`getting key: ${key}`);
+		return Reflect.get(target, key);
+    },
+    set(target, key, value) {
+		return Reflect.set(target, key, value);
+    }
+};
+
+module.exports = new Proxy(resources, handler);
